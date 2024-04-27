fdc1ecaab66d423d92e959d27d1fbfb9e4ceb09e
Allan Debastiani <allandeba@yahoo.com.br>
2024-04-27 17:26:02 -0300

    feat new wireframe layout

--- MyTheFourth/src/MyTheFourth.Frontend/wwwroot/main.js
+++ MyTheFourth/src/MyTheFourth.Frontend/wwwroot/main.js
@@ -1,4 +1,11 @@
-function toggleTheme() {    
-    var theme = document.body.dataset.bsTheme;
-    document.body.dataset.bsTheme = theme === "light" ? "dark" : "light";
-}
+window.toggleTheme = function () {
+  document.body.dataset.bsTheme = getBsTheme() === 'light' ? 'dark' : 'light';
+};
+
+window.setTheme = function (_theme) {
+  if (_theme) document.body.dataset.bsTheme = _theme;
+};
+
+window.getBsTheme = function () {
+  return document.body.dataset.bsTheme;
+};
