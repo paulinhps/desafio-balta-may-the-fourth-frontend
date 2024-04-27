7b8448816d4c2f5d2ba58b15a1921327e8bd2b91
Thiago Cajaiba <dev@thiagokj.cloud>
2024-04-27 13:52:14 -0300

    feat: componente details

--- MyTheFourth/src/MyTheFourth.Frontend/Services/Interfaces/IMoviesService.cs
+++ MyTheFourth/src/MyTheFourth.Frontend/Services/Interfaces/IMoviesService.cs
@@ -4,7 +4,6 @@ namespace MyTheFourth.Frontend.Services.Interfaces;
 
 public interface IMoviesService
 {
-
     public Task<Movie?> GetMovieAsync(string movieId);
     public Task<IEnumerable<Movie>> ListMoviesAsync(int? page = null, int? pageSize = null);
 }
